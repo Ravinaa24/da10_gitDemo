056126d5cd4ea9263efaa6ea6722c5bf04275a56
Ravinaa24 <sontakkeravina234@gmail.com>
2024-12-12 10:22:30 +0530

    test 1

---
+++ main.py
@@ -0,0 +1,8 @@
+class firstclass:
+    val1 = 0
+    val2 = 0
+
+    print('firstclass',firstclass)
+    print('firstclass.vall',firstclass.val1)
+    print('firstclass.val2',firstclass.val2)
+    print(firstclass)
